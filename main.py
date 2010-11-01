22f6323686a6cd91d4a89c4aa8c87d4cb3dd718e
Samir Faci <dev@esamir.com>
2010-11-01 15:58:36 -0500

    ChangeLog:  * Fixed python 3.x compatibility issue.  * Script now accepts identi.ca credentials from config file as well as cli parameters.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7
 import twitter
 import smtplib
 from email.parser import Parser
@@ -10,18 +10,10 @@ import datetime
 import optparse
 import getpass
 import ConfigParser
-global user
-global password
-global host
+import sys
 global api
 global configM
 
-user=''
-password=''
-host=''
-
-
-#def sendEmail(to, cc, subject, msg):
 def sendEmail(body):
     msg = MIMEMultipart()
     now = datetime.datetime.now()
@@ -36,18 +28,24 @@ def sendEmail(body):
     part1=MIMEText(body ,'plain') 
     msg.attach(part1)
     s = smtplib.SMTP(configM['email']['host'])
+
+    if(configM['email']['pass'] != ""):
+        s.login(configM['email']['user'],configM['email']['pass'])
+    else:
+        print("No logging information provided, skipping smtp auth")
+
     s.sendmail(msg['From'], msg['To'], msg.as_string())
     s.quit()
 
-    return
-    
 
 def post(msg):
     status = api.GetUserTimeline(user)
-    print [i.text for i in status]
+    print([i.text for i in status])
     api.PostUpdate(msg)
 
 def getData():
+    global configM
+    cred=configM['Tweedentica']
     day = 86400
     now = datetime.datetime.now()
     ##zero out the hours
@@ -58,14 +56,11 @@ def getData():
     else:
         yesterday= datetime.datetime.fromtimestamp(nows - day )
 
-    #yesterday= now -
-    #date = now.strftime("%m/%d/%Y")
-    status = api.GetUserTimeline(user)
+    status = api.GetUserTimeline(cred['user'])
     yesterdayL = []
     todayL = [] 
     for i in status:
         t = datetime.datetime.fromtimestamp(i.created_at_in_seconds ) 
-        #msg = "msg '%s' was create at %s"%(i.text, i.created_at)
         msg = "  * %s"%(i.text)
         if (t > yesterday and t < now  ):
             yesterdayL.append(msg)
@@ -82,23 +77,27 @@ def getData():
 
 def conn():
     global api
-    api=twitter.Api(username=user, password=password,twitterserver=host)
+    global configM
+    cred = configM['Tweedentica']
+
+    api=twitter.Api(username=cred['user'], password=cred['pass'],twitterserver=cred['host'])
     api.SetSource('python')
 
 def processConfig(config_file):
     global configM
     configM= {} # Reset configuration file.
-    print "checking config file: %s" %(config_file)
+    print("checking config file: %s" %(config_file))
     config = ConfigParser.ConfigParser()
-    #Open File
 
+    #Open File
     try:
         config.readfp(open(config_file))
     except:
-        print ("Configuration file not found")
+        print("Configuration file not found")
         sys.exit(2);
+
     try:
-        #read pgsql section.
+        #read email section.
         email  = {} 
         email['host'] = config.get('Email','smtp_host')
         email['user'] = config.get('Email','smtp_user')
@@ -110,43 +109,59 @@ def processConfig(config_file):
         email['subject'] = config.get('Email','subject')
         configM['email'] = email
     except:
-        print("Could not read psql configuration... terminating")
+        print("Could not read email configuration... terminating")
+        sys.exit(1)
+
+    try:
+        #read ident section.
+        Tweedentica  = {} 
+        Tweedentica['host'] = config.get('Tweedentica','host')
+        Tweedentica['user'] = config.get('Tweedentica','user')
+        Tweedentica['pass'] = config.get('Tweedentica','pass')
+        configM['Tweedentica'] = Tweedentica
+    except:
+        print("Could not read Tweedentica configuration... terminating")
         sys.exit(1)
 
 
 def main():
-    global user
-    global password
-    global host
     global configM
     global report
 
     configM= {}
 
     p = optparse.OptionParser()
-    p.add_option('--user', '-u', default="user",help="Twitter/Identi.ca user name")
-    p.add_option('--password', '-p', default="password",help="Twitter/Identi.ca password")
-    p.add_option('--host', '-H', default="localhost",help="API URL")
+    p.add_option('--user', '-u', default="",help="Twitter/Identi.ca user name")
+    p.add_option('--password', '-p', default="",help="Twitter/Identi.ca password")
+    p.add_option('--host', '-H', default="",help="API URL")
     p.add_option('--config','-c',default="config.ini",help="Configuration file specifying misc defaults (mostly transport / email related)")
     p.add_option('--report','-r',action="store_true", default=False,help="Only report, do not send email.")
 
     options, arguments = p.parse_args()
 
-    user=options.user
-    password=options.password
-    host=options.host
+    ##Read configuration 
+    processConfig(options.config)
+    ##if values are overloaded, use data.
+    if(options.user != ""):
+        configM['Tweedentica']['user'] = options.user
+    if(options.password != ""):
+        configM['Tweedentica']['password'] = options.password
+    if(options.host != ""):
+        configM['Tweedentica']['host'] = options.host
+
     report=options.report
 
-    processConfig(options.config)
-    if(password == "password"):
-        password=getpass.getpass("Password: ")
+    ##Allows for interactive input of pass
+    if(configM['Tweedentica']['pass'] == "" ):
+        configM['Tweedentica']['password']=getpass.getpass("Password: ")
 
     conn()
+    
     text= getData()
     if(report == True):
-        print text
+        print(text)
     else:
-        print "send email"
+        print("send email")
         sendEmail(text)
 
 if __name__ == '__main__':
